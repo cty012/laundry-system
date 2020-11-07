1641156505e62bd9ff0d0a81508cb1bc30722007
cty012 <tianyue.cao21@gmail.com>
2020-11-08 02:08:06 +0800

    Login

--- index.js
+++ index.js
@@ -10,38 +10,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.listen("8080");
 
-// file system
-class AppManager {
-    constructor(path) {
-        this.root = path;
-    }
-    readFile(relPath) {
-        return fs.readFileSync(path.join(this.root, relPath), {encoding: "utf8"});
-    }
-    getFiles(relPath) {
-        return fs.readdirSync(path.join(this.root, relPath)).filter(file => file != ".DS_Store");
-    }
-    getAppList() {
-        return this.getFiles("/");
-    }
-    getInfo(id) {
-        return JSON.parse(this.readFile(path.join(id, "/info.json")));
-    }
-    getDescription(id) {
-        return this.readFile(path.join(id, "/description.html"));
-    }
-    getNumImgs(id) {
-        return this.getFiles(path.join(id, "/carousel")).filter(file => file.toLowerCase().endsWith(".png")).length;
-    }
-}
-
-
-var appManager = new AppManager(path.join(__dirname, "/public/resources/app"));
-
 //Deal with GET request
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/views/helloworld.html"));
 });
-
-
-
